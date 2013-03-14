8309a9b3d6dfbd0c2fdeb1a7782186097acd9f1d
Tyler Neylon <tylerneylon@gmail.com>
2013-03-14 00:56:43 -0700

    Drawing and init movements of the hero.

--- pacpac/main.lua
+++ pacpac/main.lua
@@ -21,6 +21,12 @@ map = {{1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 
 tile_size = 20
 
+man_x = 10.5
+man_y = 17.5
+man_dx = -1
+man_dy = 0
+speed = 4
+
 function draw_wall(x, y)
   -- print('draw_wall(' .. x .. ', ' .. y .. ')')
   love.graphics.setColor(255, 255, 255)
@@ -28,8 +34,59 @@ function draw_wall(x, y)
                           tile_size, tile_size)
 end
 
+function draw_man()
+  love.graphics.setColor(255, 255, 0)
+  love.graphics.circle('fill', man_x * tile_size, man_y * tile_size, tile_size / 2, 10)
+end
+
 function love.draw()
   for x = 1, #map do for y = 1, #(map[1]) do
     if map[x][y] == 1 then draw_wall(x, y) end
   end end  -- Loop over x, y.
+
+  draw_man()
+end
+
+function pts_hit_by_man_at_xy(x, y)
+  local h = 0.48
+  local pts = {}
+  for dx = -1, 1, 2 do for dy = -1, 1, 2 do
+    table.insert(pts, {math.floor(x + dx * h), math.floor(y + dy * h)})
+  end end
+  return pts
+end
+
+function snap_into_place()
+  if man_dx == 0 then
+    man_x = math.floor(2 * man_x + 0.5) / 2
+  end
+  if man_dy == 0 then
+    man_y = math.floor(2 * man_y + 0.5) / 2
+  end
+end
+
+function love.update(dt)
+  --print('At update start, man_xy=(' .. man_x .. ', ' .. man_y .. ')')
+  man_x = man_x + man_dx * dt * speed
+  man_y = man_y + man_dy * dt * speed
+  snap_into_place()
+  --print('After xy update, man_xy=(' .. man_x .. ', ' .. man_y .. ')')
+
+  local pts = pts_hit_by_man_at_xy(man_x, man_y)
+  local stopped = false
+  for k, v in pairs(pts) do
+    if map[v[1]][v[2]] == 1 then
+      man_dx = 0
+      man_dy = 0
+      stopped = true
+    end
+  end
+  if stopped then snap_into_place() end
+end
+
+function love.keypressed(key)
+  local dirs = {up = {0, -1}, down = {0, 1}, left = {-1, 0}, right = {1, 0}}
+  local dir = dirs[key]
+  if dir == nil then return end
+  man_dx, man_dy = dir[1], dir[2]
 end
